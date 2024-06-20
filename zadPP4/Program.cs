86a28efd675cdb7bcfea87ce673e8fefca1b27e5
Нарек Антонян <narekantonyan2033@gmail.com>
2024-06-20 17:24:52 +0300

    Добавьте файлы проекта.

---
+++ zadPP4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+class CountdownTimer
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество секунд для обратного отсчета:");
+        int seconds;
+        if (int.TryParse(Console.ReadLine(), out seconds))
+        {
+            for (int i = seconds; i >= 0; i--)
+            {
+                Console.WriteLine($"Осталось {i} секунд...");
+                Thread.Sleep(1000); // Приостанавливаем выполнение программы на 1 секунду
+            }
+            Console.WriteLine("Обратный отсчет завершен!");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+        }
+    }
+}
+
